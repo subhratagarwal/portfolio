e32205a08ce0e951b880949d0f361ac43e2ade77
subhratagarwal <subhratagarwal1234@gmail.com>
2025-07-15 02:42:08 +0530

    Describe your changes here

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      'firebase/app',
+      'firebase/firestore',
+      'firebase/storage',
+      '@babel/runtime/helpers/createSuper'
+    ]
+  }
 })
+
+
+
