8a2121acb06bd0b202432bcb087c42ee2ffa849a
subhratagarwal <subhratagarwal1234@gmail.com>
2025-07-15 13:55:58 +0530

    Fixed portfolio build issue and updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// ✅ FIX: force Vite to optimize and prebundle babel helpers
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      // make sure any @babel/runtime imports get resolved
+      '@babel/runtime/helpers/createSuper':
+        '/node_modules/@babel/runtime/helpers/esm/createSuper.js',
+      '@babel/runtime/helpers/classCallCheck':
+        '/node_modules/@babel/runtime/helpers/esm/classCallCheck.js',
+      '@babel/runtime/helpers/inherits':
+        '/node_modules/@babel/runtime/helpers/esm/inherits.js'
+    }
+  },
+  optimizeDeps: {
+    include: [
+      '@babel/runtime/helpers/createSuper',
+      '@babel/runtime/helpers/classCallCheck',
+      '@babel/runtime/helpers/inherits'
+    ]
+  },
+  build: {
+    outDir: 'dist',
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
 })
