7c64ac353a24b7f58d73f756b4b18e00a1c22a4d
subhratagarwal <subhratagarwal1234@gmail.com>
2025-07-16 02:16:29 +0530

    Your commit message

--- src/App.jsx
+++ src/App.jsx
@@ -32,10 +32,7 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
             <center>
               <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
               <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  ABC™
-                </a>
+               
                 . All Rights Reserved.
               </span>
             </center>
@@ -53,11 +50,8 @@ const ProjectPageLayout = () => (
       <center>
         <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
         <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-            EkiZR™
-          </a>
-          . All Rights Reserved.
+          
+           All Rights Reserved.
         </span>
       </center>
     </footer>
